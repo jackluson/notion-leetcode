595544cc0d5f2d4c82819f586ab8e31d783baba3
luxuemin <luxuemin@realibox.com>
2022-07-27 16:30:30 +0800

    first commit

---
+++ solutions/204.计数质数.js
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=204 lang=javascript
+ *
+ * [204] 计数质数
+ *
+ * https://leetcode.cn/problems/count-primes/description/
+ *
+ * algorithms
+ * Medium (37.55%)
+ * Likes:    921
+ * Dislikes: 0
+ * Total Accepted:    213.3K
+ * Total Submissions: 568K
+ * Testcase Example:  '10'
+ *
+ * 给定整数 n ，返回 所有小于非负整数 n 的质数的数量 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 10
+ * 输出：4
+ * 解释：小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：n = 0
+ * 输出：0
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：n = 1
+ * 输出：0
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 0 <= n <= 5 * 10^6
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n) {
+  let primes = new Array(n).fill(1);
+  for (let i = 2; i * i < n; i++) {
+    if (primes[i]) {
+      for (let j = 2; j * i < n; j++) {
+        primes[j * i] = 0;
+      }
+    }
+  }
+  let count = 0;
+  for (let i = 2; i < n; i++) {
+    if (primes[i] === 1) {
+      count++;
+    }
+  }
+  return count;
+};
+// @lc code=end
---
+++ solutions/283.移动零.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=283 lang=javascript
+ *
+ * [283] 移动零
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let slow = 0;
+  let fast = 0;
+  while (fast < nums.length) {
+    if (nums[fast] !== 0) {
+      nums[slow] = nums[fast];
+      slow++;
+    }
+    fast++;
+  }
+  while (slow < nums.length) {
+    nums[slow] = 0;
+    slow++;
+  }
+  //return slow
+};
+// @lc code=end
