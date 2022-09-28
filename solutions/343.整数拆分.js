0b7632193e5285584d8e226cc368b110038624a1
luxuemin <luxuemin@realibox.com>
2022-09-28 16:48:51 +0800

    solution: :gift: add integer-break

---
+++ solutions/343.整数拆分.js
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=343 lang=javascript
+ *
+ * [343] 整数拆分
+ *
+ * https://leetcode.cn/problems/integer-break/description/
+ *
+ * algorithms
+ * Medium (62.11%)
+ * Likes:    942
+ * Dislikes: 0
+ * Total Accepted:    206K
+ * Total Submissions: 331.8K
+ * Testcase Example:  '2'
+ *
+ * 给定一个正整数 n ，将其拆分为 k 个 正整数 的和（ k >= 2 ），并使这些整数的乘积最大化。
+ *
+ * 返回 你可以获得的最大乘积 。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: n = 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1。
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: n = 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 2 <= n <= 58
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function (n) {
+  let dp = [0, 0, 1];
+  for (let i = 3; i <= n; i++) {
+    dp[i] = Math.max(
+      Math.max(dp[i - 2], i - 2) * 2,
+      Math.max(dp[i - 3], i - 3) * 3
+    );
+  }
+  return dp[n];
+};
+// @lc code=end
