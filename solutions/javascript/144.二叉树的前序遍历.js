15b2c5f0c145b03a0cf765c6d91ef5ecf9ceb019
luxuemin <luxuemin@realibox.com>
2022-10-12 19:17:19 +0800

    solutions: :gift: add 144.binary-tree-preorder-traversal

---
+++ solutions/javascript/144.二叉树的前序遍历.js
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ *
+ * https://leetcode.cn/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (71.27%)
+ * Likes:    923
+ * Dislikes: 0
+ * Total Accepted:    723.6K
+ * Total Submissions: 1M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,2,3]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ *
+ *
+ * 示例 4：
+ *
+ *
+ * 输入：root = [1,2]
+ * 输出：[1,2]
+ *
+ *
+ * 示例 5：
+ *
+ *
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目在范围 [0, 100] 内
+ * -100
+ *
+ *
+ *
+ *
+ * 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  if (root === null) return [];
+  return [
+    root.val,
+    ...preorderTraversal(root.left),
+    ...preorderTraversal(root.right),
+  ];
+};
+// @lc code=end
