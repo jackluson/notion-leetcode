dcaf23aa6299f22a1071a35fd4c9a781722799f9
luxuemin <luxuemin@realibox.com>
2022-10-17 15:11:30 +0800

    solution: :gift: add 169.majority-element

---
+++ solutions/javascript/169.多数元素_hashmap.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=javascript
+ *
+ * [169] 多数元素
+ *
+ * https://leetcode.cn/problems/majority-element/description/
+ *
+ * algorithms
+ * Easy (66.94%)
+ * Likes:    1593
+ * Dislikes: 0
+ * Total Accepted:    604.8K
+ * Total Submissions: 904.1K
+ * Testcase Example:  '[3,2,3]'
+ *
+ * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums = [3,2,3]
+ * 输出：3
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums = [2,2,1,1,1,2,2]
+ * 输出：2
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == nums.length
+ * 1 <= n <= 5 * 10^4
+ * -10^9 <= nums[i] <= 10^9
+ *
+ *
+ *
+ *
+ * 进阶：尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  const elementMap = new Map();
+  for (const num of nums) {
+    const curCount = (elementMap.get(num) || 0) + 1;
+    elementMap.set(num, curCount);
+    if (curCount > nums.length / 2) {
+      return num;
+    }
+  }
+};
+// @lc code=end
