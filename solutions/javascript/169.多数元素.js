7530f0b1c0a05c70b9a065442c82de25c2c280f6
jackluson <luxuemin2108@gmail.com>
2023-01-18 17:38:21 +0800

    feat: review 169.majority-element

--- solutions/javascript/169.多数元素.js
+++ solutions/javascript/169.多数元素.js
@@ -14,37 +14,37 @@
  * Testcase Example:  '[3,2,3]'
  *
  * 给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
- *
+ * 
  * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
- *
- *
- *
+ * 
+ * 
+ * 
  * 示例 1：
- *
- *
+ * 
+ * 
  * 输入：nums = [3,2,3]
  * 输出：3
- *
+ * 
  * 示例 2：
- *
- *
+ * 
+ * 
  * 输入：nums = [2,2,1,1,1,2,2]
  * 输出：2
- *
- *
- *
+ * 
+ * 
+ * 
  * 提示：
- *
- *
+ * 
+ * 
  * n == nums.length
  * 1 <= n <= 5 * 10^4
  * -10^9 <= nums[i] <= 10^9
- *
- *
- *
- *
+ * 
+ * 
+ * 
+ * 
  * 进阶：尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。
- *
+ * 
  */
 
 // @lc code=start
@@ -52,20 +52,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function (nums) {
-  let count = 0;
-  let candidate = nums[0];
-  for (const num of nums) {
-    if (num === candidate) {
-      count++;
+var majorityElement = function(nums) {
+  let candidate = nums[0]
+  let count = 1
+  for(let i = 1; i < nums.length; i++){
+    if(candidate === nums[i]){
+      count++
     } else {
-      if (count === 1) {
-        candidate = num;
+      if(count === 1){
+        candidate = nums[i]
       } else {
-        count--;
+        count--
       }
     }
   }
-  return candidate;
+  return candidate
 };
 // @lc code=end
+
