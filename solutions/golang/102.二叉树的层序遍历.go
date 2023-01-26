ef97106d60453223e78b7951907c3cde7bdc38c5
jackluson <luxuemin2108@gmail.com>
2023-01-26 12:50:27 +0800

    feat: add 102.binary-tree-level-order-traversal

---
+++ solutions/golang/102.二叉树的层序遍历.go
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=golang
+ *
+ * [102] 二叉树的层序遍历
+ *
+ * https://leetcode.cn/problems/binary-tree-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (65.08%)
+ * Likes:    1553
+ * Dislikes: 0
+ * Total Accepted:    734.7K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[9,20],[15,7]]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1]
+ * 输出：[[1]]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：root = []
+ * 输出：[]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点数目在范围 [0, 2000] 内
+ * -1000 <= Node.val <= 1000
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+package main
+
+func levelOrder(root *TreeNode) [][]int {
+	ans := [][]int{}
+	if root == nil {
+		return ans
+	}
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		lastIndex := len(queue) - 1
+		cur := []int{}
+		next := []*TreeNode{}
+		for i := 0; lastIndex >= i; i++ {
+			item := queue[i]
+			cur = append(cur, item.Val)
+			if item.Left != nil {
+				next = append(next, item.Left)
+			}
+			if item.Right != nil {
+				next = append(next, item.Right)
+			}
+		}
+		ans = append(ans, cur)
+		queue = next
+	}
+	return ans
+}
+
+// @lc code=end
