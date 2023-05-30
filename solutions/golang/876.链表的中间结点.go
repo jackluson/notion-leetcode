84580554d140c955864e3f4f2cc2fa17a79439e1
jackluson <luxuemin2108@gmail.com>
2023-05-30 17:21:16 +0800

    feat: add 876.middle-of-the-linked-list

---
+++ solutions/golang/876.链表的中间结点.go
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=golang
+ *
+ * [876] 链表的中间结点
+ *
+ * https://leetcode.cn/problems/middle-of-the-linked-list/description/
+ *
+ * algorithms
+ * Easy (70.55%)
+ * Likes:    892
+ * Dislikes: 0
+ * Total Accepted:    394.3K
+ * Total Submissions: 561.7K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给你单链表的头结点 head ，请你找出并返回链表的中间结点。
+ *
+ * 如果有两个中间结点，则返回第二个中间结点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[3,4,5]
+ * 解释：链表只有一个中间结点，值为 3 。
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：head = [1,2,3,4,5,6]
+ * 输出：[4,5,6]
+ * 解释：该链表有两个中间结点，值分别为 3 和 4 ，返回第二个结点。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 链表的结点数范围是 [1, 100]
+ * 1 <= Node.val <= 100
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+	if head.Next == nil {
+		return head
+	}
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+// @lc code=end
+
