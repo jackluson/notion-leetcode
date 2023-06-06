516759d0640e048e1f8ffdade1f267268445c463
jackluson <luxuemin2108@gmail.com>
2023-06-06 16:31:30 +0800

    feat(golang): add 70.climbing-stairs

---
+++ solutions/golang/70.爬楼梯.go
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=golang
+ *
+ * [70] 爬楼梯
+ *
+ * https://leetcode.cn/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (53.96%)
+ * Likes:    2971
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '2'
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 2
+ * 输出：2
+ * 解释：有两种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶
+ * 2. 2 阶
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：n = 3
+ * 输出：3
+ * 解释：有三种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶 + 1 阶
+ * 2. 1 阶 + 2 阶
+ * 3. 2 阶 + 1 阶
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= n <= 45
+ *
+ *
+ */
+package main
+
+// @lc code=start
+func climbStairs(n int) int {
+	dp := []int{1, 2}
+	for i := 2; i < n; i++ {
+		dp = append(dp, dp[i-1]+dp[i-2])
+	}
+	return dp[n-1]
+}
+
+// @lc code=end
